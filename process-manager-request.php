b41c7a00ec94aafb33b57595886e2b4039753d19
Mitchell Anton <antonm@student.unimelb.edu.au>
2021-10-17 13:41:33 +1100

    Successfully implemented functionality to receive all applications.

---
+++ process-manager-request.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <meta name="author" content="Mitchell Anton" />
+    <title>Result | Anton & Turing Technologies</title>
+    <link href="styles/style.css" rel="stylesheet" />
+</head>
+
+<body>
+    <?php
+        // TODO: Style table.
+        require_once("settings.php");
+        $connection = mysqli_connect($host, $user, $pwd, $sql_db) or die("<p>Database connection failure.</p>");
+        $sqlTable = "eoi";
+
+        // The manager pressed the button to get all form applications.
+        if (isset($_GET["get_all_applications"])) {
+            $query = "select * from eoi";
+            $result = mysqli_query($connection, $query);
+
+            if (mysqli_num_rows($result) == 0) {
+                echo "<p>There are no job applications yet.</p>";
+            }
+            else {
+                echo "<table class='manager-request'>\n";
+
+                // Table headers
+                echo "<tr>\n"
+                    . "<th>eoi Number</th>\n"
+                    . "<th>Job Reference Number</th>\n"
+                    . "<th>First Name</th>\n"
+                    . "<th>Last Name</th>\n"
+                    . "<th>Street Address</th>\n"
+                    . "<th>Suburb</th>\n"
+                    . "<th>State</th>\n"
+                    . "<th>Postcode</th>\n"
+                    . "<th>Email</th>\n"
+                    . "<th>Phone Number</th>\n"
+                    . "<th>Skills</th>\n"
+                    . "<th>Other Skills</th>\n"
+                    . "<th>Status</th>\n";
+
+                // Table content.
+                while ($row = mysqli_fetch_assoc($result)) {
+                    echo "<tr>\n"
+                        . "<th>" . $row["eoiNumber"] . "</th>\n"
+                        . "<th>" . $row["JobReferenceNumber"] . "</th>\n"
+                        . "<th>" . $row["FirstName"] . "</th>\n"
+                        . "<th>" . $row["LastName"] . "</th>\n"
+                        . "<th>" . $row["StreetAddress"] . "</th>\n"
+                        . "<th>" . $row["Suburb"] . "</th>\n"
+                        . "<th>" . $row["StateLocation"] . "</th>\n"
+                        . "<th>" . $row["Postcode"] . "</th>\n"
+                        . "<th>" . $row["EmailAddress"] . "</th>\n"
+                        . "<th>" . $row["PhoneNumber"] . "</th>\n"
+                        . "<th>" . $row["Skills"] . "</th>\n"
+                        . "<th>" . $row["OtherSkills"] . "</th>\n"
+                        . "<th>" . $row["Status"] . "</th>\n";
+                }
+                echo "</table>\n";
+                mysqli_free_result($result);
+            }
+        }
+    ?>
+</body>
