6d8a46553b0b3098460136240ab309a8aa95c48d
Mitchell Anton <antonm@student.unimelb.edu.au>
2021-10-17 23:51:28 +1100

    Changed pattern for job reference number. Also succesfully added feature for manager to search eoi's by job reference number.

--- manage.php
+++ manage.php
@@ -19,6 +19,8 @@
         <input class="form_action" type="submit" name="get_all_applications" value="Get All Applications">
     </form>
     <form id="get_eois_given_ref" method="get" action="process-manager-request.php">
+        <label for="reference_number">Job Reference Number</label>
+        <input type="text" name="reference_number" id="reference_number" required="required" pattern="[a-zA-Z0-9]{5}" />
         <input class="form_action" type="submit" name="get_eois_given_ref" value="Get Applications">
     </form>
     <hr />
