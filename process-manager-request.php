28a8115bd467aa84e93a09e41b1f3a06b162ebfa
Mitchell Anton <antonm@student.unimelb.edu.au>
2021-10-18 00:13:30 +1100

    Changed my mind and added client side validation for manage.php. Got rid of ALL server side validation although again, I shall check with tutor if this is the correct thing to do.

--- process-manager-request.php
+++ process-manager-request.php
@@ -79,64 +79,54 @@
         function getEOIsGivenRef() {
             global $connection;
             global $sqlTable;
-            $valid = true;
+            $jobReferenceNumber = $_GET["reference_number"];
 
-            // A job reference number has been entered.
-            if (isset($_GET["reference_number"])) {
-                $jobReferenceNumber = sanitise_input($_GET["reference_number"]);
+            // DON'T FORGET SINGLE QUOTATION MARKS!
+            $query = "select * from eoi where JobReferenceNumber = '$jobReferenceNumber'";
+            $result = mysqli_query($connection, $query);
+
+            // Note: !$result is for when query is invalid.
+            if (!$result || mysqli_num_rows($result) == 0) {
+                echo "<p>There are no job applications for this position yet or this job does not exist.</p>";
             }
             else {
-                $valid = false;
-            }
+                echo "<table class='manager_request'>\n";
 
-            if ($valid) {
-                // DON'T FORGET SINGLE QUOTATION MARKS!
-                $query = "select * from eoi where JobReferenceNumber = '$jobReferenceNumber'";
-                $result = mysqli_query($connection, $query);
-
-                // Note: !$result is for when query is invalid.
-                if (!$result || mysqli_num_rows($result) == 0) {
-                    echo "<p>There are no job applications for this position yet or this job does not exist.</p>";
-                }
-                else {
-                    echo "<table class='manager_request'>\n";
-
-                    // Table headers
-                    echo "<tr>\n"
-                        . "<th>eoi Number</th>\n"
-                        . "<th>Job Reference Number</th>\n"
-                        . "<th>First Name</th>\n"
-                        . "<th>Last Name</th>\n"
-                        . "<th>Street Address</th>\n"
-                        . "<th>Suburb</th>\n"
-                        . "<th>State</th>\n"
-                        . "<th>Postcode</th>\n"
-                        . "<th>Email</th>\n"
-                        . "<th>Phone Number</th>\n"
-                        . "<th>Skills</th>\n"
-                        . "<th>Other Skills</th>\n"
-                        . "<th>Status</th>\n";
+                // Table headers
+                echo "<tr>\n"
+                    . "<th>eoi Number</th>\n"
+                    . "<th>Job Reference Number</th>\n"
+                    . "<th>First Name</th>\n"
+                    . "<th>Last Name</th>\n"
+                    . "<th>Street Address</th>\n"
+                    . "<th>Suburb</th>\n"
+                    . "<th>State</th>\n"
+                    . "<th>Postcode</th>\n"
+                    . "<th>Email</th>\n"
+                    . "<th>Phone Number</th>\n"
+                    . "<th>Skills</th>\n"
+                    . "<th>Other Skills</th>\n"
+                    . "<th>Status</th>\n";
     
-                    // Table content.
-                    while ($row = mysqli_fetch_assoc($result)) {
-                        echo "<tr>\n"
-                            . "<th>" . $row["eoiNumber"] . "</th>\n"
-                            . "<th>" . $row["JobReferenceNumber"] . "</th>\n"
-                            . "<th>" . $row["FirstName"] . "</th>\n"
-                            . "<th>" . $row["LastName"] . "</th>\n"
-                            . "<th>" . $row["StreetAddress"] . "</th>\n"
-                            . "<th>" . $row["Suburb"] . "</th>\n"
-                            . "<th>" . $row["StateLocation"] . "</th>\n"
-                            . "<th>" . $row["Postcode"] . "</th>\n"
-                            . "<th>" . $row["EmailAddress"] . "</th>\n"
-                            . "<th>" . $row["PhoneNumber"] . "</th>\n"
-                            . "<th>" . $row["Skills"] . "</th>\n"
-                            . "<th>" . $row["OtherSkills"] . "</th>\n"
-                            . "<th>" . $row["Status"] . "</th>\n";
-                    }
-                    echo "</table>\n";
-                    mysqli_free_result($result);
+                // Table content.
+                while ($row = mysqli_fetch_assoc($result)) {
+                    echo "<tr>\n"
+                        . "<th>" . $row["eoiNumber"] . "</th>\n"
+                        . "<th>" . $row["JobReferenceNumber"] . "</th>\n"
+                        . "<th>" . $row["FirstName"] . "</th>\n"
+                        . "<th>" . $row["LastName"] . "</th>\n"
+                        . "<th>" . $row["StreetAddress"] . "</th>\n"
+                        . "<th>" . $row["Suburb"] . "</th>\n"
+                        . "<th>" . $row["StateLocation"] . "</th>\n"
+                        . "<th>" . $row["Postcode"] . "</th>\n"
+                        . "<th>" . $row["EmailAddress"] . "</th>\n"
+                        . "<th>" . $row["PhoneNumber"] . "</th>\n"
+                        . "<th>" . $row["Skills"] . "</th>\n"
+                        . "<th>" . $row["OtherSkills"] . "</th>\n"
+                        . "<th>" . $row["Status"] . "</th>\n";
                 }
+                echo "</table>\n";
+                mysqli_free_result($result);
             }
         }
 
