a19abe6fcfc50c3fde4a1aa18b683a6e81b9c1b0
Mitchell Anton <antonm@student.unimelb.edu.au>
2021-10-17 13:41:33 +1100

    Successfully implemented functionality to receive all applications.

--- manage.php
+++ manage.php
@@ -15,6 +15,9 @@
         $page = "managerPage";
         include_once("menu.inc"); 
     ?>
+    <form id="get_all_applications" method="get" action="process-manager-request.php">
+        <input class="form_action" type="submit" name="get_all_applications" value="Get All Applications">
+    </form>
     <hr />
     <?php
         include_once("footer.inc");
