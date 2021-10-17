90a03564364a6a4f9fbf0036b3839cac4d346104
Mitchell Anton <antonm@student.unimelb.edu.au>
2021-10-18 00:02:05 +1100

    Style changes on manage.php

--- manage.php
+++ manage.php
@@ -15,14 +15,34 @@
         $page = "managerPage";
         include_once("menu.inc"); 
     ?>
-    <form id="get_all_applications" method="get" action="process-manager-request.php">
-        <input class="form_action" type="submit" name="get_all_applications" value="Get All Applications">
-    </form>
-    <form id="get_eois_given_ref" method="get" action="process-manager-request.php">
-        <label for="reference_number">Job Reference Number</label>
-        <input type="text" name="reference_number" id="reference_number" required="required" pattern="[a-zA-Z0-9]{5}" />
-        <input class="form_action" type="submit" name="get_eois_given_ref" value="Get Applications">
-    </form>
+    <fieldset class="manager_request">
+        <legend>Get All Job Applications</legend>
+        <form id="get_all_applications" method="get" action="process-manager-request.php">
+            <input class="form_action" type="submit" name="get_all_applications" value="Get All Applications">
+        </form>
+    </fieldset>
+
+    <fieldset class="manager_request">
+        <legend>Get Job Applications by Job Reference Number</legend>
+        <form id="get_eois_given_ref" method="get" action="process-manager-request.php">
+            <label for="reference_number">Job Reference Number</label>
+            <input type="text" name="reference_number" id="reference_number" required="required" pattern="[a-zA-Z0-9]{5}" />
+            <input class="form_action" type="submit" name="get_eois_given_ref" value="Get Applications">
+        </form>
+    </fieldset>
+
+    <fieldset class="manager_request">
+        <legend>Get Job Applications By Name</legend>
+        <form id="get_eois_given_name" method="get" action="process-manager-request.php">
+            <!-- Note that we only require one of first name and last name, so we will not make the fields required.
+                 This is why the pattern here is slightly different from the pattern in apply.php. -->
+            <label for="first_name">First Name</label>
+            <input type="text" name="first_name" id="first_name" pattern="[A-Za-z]{0,20}" />
+            <label for="last_name">Last Name</label>
+            <input type="text" name="last_name" id="last_name" pattern="[A-Za-z]{0,20}" />
+            <input class="form_action" type="submit" name="get_eois_given_name" value="Get Applications">
+        </form>
+    </fieldset>
     <hr />
     <?php
         include_once("footer.inc");
