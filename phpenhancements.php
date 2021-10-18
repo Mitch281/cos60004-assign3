f06b06e3026da6ff116d84a6e10f9a08dbca14c7
Mitchell Anton <antonm@student.unimelb.edu.au>
2021-10-18 21:41:43 +1100

    Added phpenhancements page. Changed nav bar accordingly.

---
+++ phpenhancements.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="description" content="Enhancements" />
+    <meta name="keywords" content="Enhancements, php, cos60004, assignment 3" />
+    <meta name="author" content="Mitchell Anton" />
+    <title>php enhancements | Anton & Turing Technologies</title>
+    <link href="styles/style.css" rel="stylesheet" />
+</head>
+
+<body>
+    <?php
+        $page = "phpEnhancementsPage";
+        include_once("menu.inc");
+    ?>
+    <hr />
+    <?php  
+        include_once("footer.inc");
+    ?>
+</body>
