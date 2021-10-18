07c3e04cb8841b54eac3b5f0b2b201b8b78c3215
Mitchell Anton <antonm@student.unimelb.edu.au>
2021-10-18 17:52:57 +1100

    Manager login is working (hopefully there aren't any bugs I have missed).

---
+++ process-manager-login.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="description" content="Jobs available and their descriptions." />
+    <meta name="keywords" content="hiring, jobs, vacant" />
+    <meta name="author" content="Mitchell Anton" />
+    <title></title>
+    <link href="styles/style.css" rel="stylesheet" />
+</head>
+
+<body>
+    <?php
+        require_once("settings.php");
+        $connection = mysqli_connect($host, $user, $pwd, $sql_db) or die("<p>Database connection failure.</p>");
+        $sqlTable = "ManagerLogin";
+        
+        // Boolean to check if the username and password entered is valid.
+        $valid = false;
+        $usernameError = "";
+        $passwordError = "";
+
+        $username = $_GET["username"];
+        $password = $_GET["password"];
+
+        // If the username exists, then this shall only return one username.
+        $checkExistQuery = "select * from $sqlTable where username = '$username'";
+
+        $result = mysqli_query($connection, $checkExistQuery);
+
+        // Query successful and username exists.
+        if ($result && mysqli_num_rows($result) != 0) {
+            $passwordQuery = "select * from $sqlTable where password = '$password'";
+            $result = mysqli_query($connection, $passwordQuery);
+            while ($row = mysqli_fetch_assoc($result)) {
+                // The candidate username is any username where the password matches the password the user has entered.
+                // This is then compared to the username entered until there is a match.
+                $candidateUsername = $row["Username"];
+                if ($username == $candidateUsername) {
+                    $valid = true;
+                    break;
+                }
+            }
+            mysqli_free_result($result);
+        }
+
+        if ($valid) {
+            header("location: manage.php");
+        }
+        else {
+            echo "<p>Invalid username and/or password</p>";
+        }
+        mysqli_close($connection);
+    ?>
+</body>
